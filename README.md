c90d3d4f83f143f5f22688d7b021bd8e1f600948
nngogol <nngogol09@gmail.com>
2018-02-22 13:20:00 +0200

    added comment

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
 # Алгоритм floyd_warshall на графах
 ## ЗАЧЕМ?
-##### Прост)
+##### Прост) (На самом дела, это довольно-таки интересный алгоритм, потому что он за n^3 дает придельно много информации о графе.)
 ## КАК УСТАНОВИТЬ?
-##### делает pip install networkx
-##### заменяет C:\Program Files\Python36\Lib\site-packages\networkx\algorithms\shortest_paths\dense.py на мой файлик dense.py
-## Пример можно, а то не понятно...
+##### pip install networkx
+##### replace C:\Program Files\Python36\Lib\site-packages\networkx\algorithms\shortest_paths\dense.py -> dense.py
+## КАК ИСПОЛЬЗОВАТЬ?
+### Пример:
 ```
 import networkx as nx
 G = nx.gnm_random_graph(15,50)
 nx.algorithms.shortest_paths.dense.floyd_warshall(G, do_csv=True)
+
+# Альтернатива
+# ставим dense.py в корень проета
+import dense as d
+G = nx.gnm_random_graph(15,50)
+d.floyd_warshall(G, do_csv=True)
 ```
-##### Код вверху создаст файлик в текущей папке, где будут ваши данные :D
+##### после floyd_warshall() в корне папки будет файлик
--- just_a_example.py
+++ just_a_example.py
@@ -1,3 +1,40 @@
 import networkx as nx
-G = nx.gnm_random_graph(15,50)
-resu = nx.algorithms.shortest_paths.dense.floyd_warshall(G, do_csv=True)
+
+#               #############################
+# 1.             Choose a graph
+
+# 		Create path-graph
+# 		Создает граф-цепь на 6 вершин
+# G=nx.path_graph(6)
+# OR 	Create graph(v,e), where v - # vertex | e - # edges
+# 		Создает граф с v вершинами и e ребрами
+# G = nx.gnm_random_graph(15,50)
+
+#         OR
+
+# Read graph from file
+# Читаем граф из файла
+G = nx.read_edgelist("1.test", nodetype=int)
+# Формат файла: откуда{пробел}куда
+# Детали: https://networkx.github.io/documentation/networkx-1.9/reference/readwrite.edgelist.html
+# 1 2
+# 3 1
+# 2 4
+# 3 4
+
+# Show graph
+# print(G.edges)
+
+
+#               #############################
+# 2.               Do floyd Warshall alg
+# 2.               Делаем дела :)
+nx.algorithms.shortest_paths.dense.floyd_warshall(G, do_csv=True)
+
+
+
+#               #############################
+# 3. (optional)    Export graph
+
+# export
+# nx.write_edgelist(G, "test.edgelist")
